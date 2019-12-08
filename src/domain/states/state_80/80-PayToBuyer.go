ea97f040bc5ca63b6040b36038e00a2b2fc15ae0
Sina.Tadayon <sina.tadayon@faza.io>
2019-12-08 14:20:13 +0330

    Implement 80,90 states

--- src/domain/states/state_80/80-PayToBuyer.go
+++ src/domain/states/state_80/80-PayToBuyer.go
@@ -2,15 +2,20 @@ package state_80
 
 import (
 	"context"
+	"gitlab.faza.io/go-framework/logger"
 	"gitlab.faza.io/order-project/order-service/domain/actions"
+	stock_action "gitlab.faza.io/order-project/order-service/domain/actions/stock"
+	system_action "gitlab.faza.io/order-project/order-service/domain/actions/system"
+	"gitlab.faza.io/order-project/order-service/domain/models/entities"
 	"gitlab.faza.io/order-project/order-service/domain/states"
 	"gitlab.faza.io/order-project/order-service/infrastructure/frame"
+	"gitlab.faza.io/order-project/order-service/infrastructure/global"
+	"time"
 )
 
 const (
 	stepName  string = "Pay_To_Buyer"
 	stepIndex int    = 80
-	//Canceled         = "CANCELED"
 )
 
 type payToBuyerState struct {
@@ -34,110 +39,73 @@ func NewValueOf(base *states.BaseStateImpl, params ...interface{}) states.IState
 }
 
 func (state payToBuyerState) Process(ctx context.Context, iFrame frame.IFrame) {
-	panic("implementation required")
+	if iFrame.Header().KeyExists(string(frame.HeaderSubpackage)) {
+		subpkg, ok := iFrame.Header().Value(string(frame.HeaderSubpackage)).(*entities.Subpackage)
+		if !ok {
+			logger.Err("iFrame.Header() not a subpackage, frame: %v, %s state ", iFrame, state.Name())
+			return
+		}
+
+		var releaseStockAction *entities.Action
+		if err := state.releasedStock(ctx, subpkg); err != nil {
+			releaseStockAction = &entities.Action{
+				Name:      stock_action.Release.ActionName(),
+				Type:      actions.Stock.ActionName(),
+				Result:    string(states.ActionFail),
+				Reasons:   nil,
+				CreatedAt: time.Now().UTC(),
+			}
+		} else {
+			releaseStockAction = &entities.Action{
+				Name:      stock_action.Release.ActionName(),
+				Type:      actions.Stock.ActionName(),
+				Result:    string(states.ActionSuccess),
+				Reasons:   nil,
+				CreatedAt: time.Now().UTC(),
+			}
+		}
+
+		nextToAction := &entities.Action{
+			Name:      system_action.NextToState.ActionName(),
+			Type:      actions.System.ActionName(),
+			Result:    string(states.ActionSuccess),
+			Reasons:   nil,
+			CreatedAt: time.Now().UTC(),
+		}
+
+		state.UpdateSubPackage(ctx, subpkg, releaseStockAction)
+		state.UpdateSubPackage(ctx, subpkg, nextToAction)
+		subPkgUpdated, err := global.Singletons.SubPkgRepository.Update(ctx, *subpkg)
+		if err != nil {
+			logger.Err("SubPkgRepository.Update in %s state failed, orderId: %d, sellerId: %d, itemId: %d, error: %s",
+				state.Name(), subpkg.OrderId, subpkg.SellerId, subpkg.ItemId, err.Error())
+		} else {
+			logger.Audit("Cancel by seller success, orderId: %d, sellerId: %d, itemId: %d", subpkg.OrderId, subpkg.SellerId, subpkg.ItemId)
+			state.StatesMap()[state.Actions()[0]].Process(ctx, frame.FactoryOf(iFrame).SetBody(subPkgUpdated).Build())
+		}
+	} else {
+		logger.Err("iFrame.Header() not a subpackage or sellerId not found, state: %s iframe: %v", state.Name(), iFrame)
+	}
 }
 
-//func (payToBuyer payToBuyerState) ProcessOrder(ctx context.Context, order entities.Order, itemsId []uint64, param interface{}) future.IFuture {
-//
-//	logger.Audit("Pay to Buyer step, orderId: %d", order.OrderId)
-//
-//	if len(order.Items) == len(itemsId) {
-//		payToBuyer.UpdateAllOrderStatus(ctx, &order, itemsId, states.OrderClosedStatus, false)
-//	} else {
-//		payToBuyer.UpdateAllOrderStatus(ctx, &order, itemsId, states.OrderInProgressStatus, false)
-//	}
-//
-//	payToBuyer.updateOrderItemsProgress(ctx, &order, itemsId, Canceled, true, states.OrderClosedStatus)
-//	if err := payToBuyer.persistOrder(ctx, &order); err != nil {
-//		returnChannel := make(chan future.IDataFuture, 1)
-//		defer close(returnChannel)
-//		returnChannel <- future.IDataFuture{Data: nil, Ex: future.FutureError{Code: future.InternalError, Reason: "Unknown Error"}}
-//		return future.NewFuture(returnChannel, 1, 1)
-//	}
-//	returnChannel := make(chan future.IDataFuture, 1)
-//	defer close(returnChannel)
-//	returnChannel <- future.IDataFuture{Data: nil, Ex: nil}
-//	return future.NewFuture(returnChannel, 1, 1)
-//}
-//
-//func (payToBuyer payToBuyerState) persistOrder(ctx context.Context, order *entities.Order) error {
-//	_, err := global.Singletons.OrderRepository.Save(*order)
-//	if err != nil {
-//		logger.Err("OrderRepository.Save in %s step failed, order: %v, error: %s", payToBuyer.Name(), order, err.Error())
-//	}
-//	return err
-//}
-//
-//func (payToBuyer payToBuyerState) updateOrderItemsProgress(ctx context.Context, order *entities.Order, itemsId []uint64, action string, result bool, itemStatus string) {
-//
-//	findFlag := false
-//	if itemsId != nil && len(itemsId) > 0 {
-//		for _, id := range itemsId {
-//			findFlag = false
-//			for i := 0; i < len(order.Items); i++ {
-//				if order.Items[i].ItemId == id {
-//					payToBuyer.doUpdateOrderItemsProgress(ctx, order, i, action, result, itemStatus)
-//					findFlag = true
-//					break
-//				}
-//			}
-//
-//			if findFlag == false {
-//				logger.Err("%s received itemId %d not exist in order, orderId: %d", payToBuyer.Name(), id, order.OrderId)
-//			}
-//		}
-//	} else {
-//		for i := 0; i < len(order.Items); i++ {
-//			payToBuyer.doUpdateOrderItemsProgress(ctx, order, i, action, result, itemStatus)
-//		}
-//	}
-//}
-//
-//func (payToBuyer payToBuyerState) doUpdateOrderItemsProgress(ctx context.Context, order *entities.Order, index int,
-//	actionName string, result bool, itemStatus string) {
-//
-//	order.Items[index].Status = itemStatus
-//	order.Items[index].UpdatedAt = time.Now().UTC()
-//
-//	length := len(order.Items[index].Progress.StepsHistory) - 1
-//
-//	if order.Items[index].Progress.StepsHistory[length].ActionHistory == nil || len(order.Items[index].Progress.StepsHistory[length].ActionHistory) == 0 {
-//		order.Items[index].Progress.StepsHistory[length].ActionHistory = make([]entities.Action, 0, 5)
-//	}
-//
-//	action := entities.Action{
-//		Name:      actionName,
-//		Result:    result,
-//		CreatedAt: order.Items[index].UpdatedAt,
-//	}
-//
-//	order.Items[index].Progress.StepsHistory[length].ActionHistory = append(order.Items[index].Progress.StepsHistory[length].ActionHistory, action)
-//}
-//
-////
-////import (
-////	"github.com/Shopify/sarama"
-////	"gitlab.faza.io/order-project/order-service"
-////)
-////
-////// TODO: must be implemented
-////func PayToBuyerMessageValidate(message *sarama.ConsumerMessage) (*sarama.ConsumerMessage, error) {
-////	mess, err := main.CheckOrderKafkaAndMongoStatus(message, main.PayToBuyer)
-////	if err != nil {
-////		return mess, err
-////	}
-////	return message, nil
-////}
-////
-////func PayToBuyerAction(message *sarama.ConsumerMessage) error {
-////
-////	err := PayToBuyerProduce("", []byte{})
-////	if err != nil {
-////		return err
-////	}
-////	return nil
-////}
-////
-////func PayToBuyerProduce(topic string, payload []byte) error {
-////	return nil
-////}
+func (state payToBuyerState) releasedStock(ctx context.Context, subpackage *entities.Subpackage) error {
+
+	var inventories = make(map[string]int, 32)
+	for z := 0; z < len(subpackage.Items); z++ {
+		item := subpackage.Items[z]
+		inventories[item.InventoryId] = int(item.Quantity)
+	}
+
+	iFuture := global.Singletons.StockService.BatchStockActions(ctx, inventories,
+		stock_action.New(stock_action.Release))
+	futureData := iFuture.Get()
+	if futureData.Error() != nil {
+		logger.Err("Reserved stock from stockService failed, state: %s, orderId: %d, sellerId: %d, itemId: %d, error: %s",
+			state.Name(), subpackage.OrderId, subpackage.SellerId, subpackage.ItemId, futureData.Error())
+		return futureData.Error().Reason()
+	}
+
+	logger.Audit("Release stock success, state: %s, orderId: %d, sellerId: %d, itemId: %d",
+		state.Name(), subpackage.OrderId, subpackage.SellerId, subpackage.ItemId)
+	return nil
+}
--- src/domain/states/state_90/90-PayToSeller.go
+++ src/domain/states/state_90/90-PayToSeller.go
@@ -2,15 +2,19 @@ package state_90
 
 import (
 	"context"
+	"gitlab.faza.io/go-framework/logger"
 	"gitlab.faza.io/order-project/order-service/domain/actions"
+	stock_action "gitlab.faza.io/order-project/order-service/domain/actions/stock"
+	"gitlab.faza.io/order-project/order-service/domain/models/entities"
 	"gitlab.faza.io/order-project/order-service/domain/states"
 	"gitlab.faza.io/order-project/order-service/infrastructure/frame"
+	"gitlab.faza.io/order-project/order-service/infrastructure/global"
+	"time"
 )
 
 const (
 	stepName  string = "Pay_To_Seller"
 	stepIndex int    = 90
-	//Delivered        = "DELIVERED"
 )
 
 type payToSellerState struct {
@@ -34,109 +38,63 @@ func NewValueOf(base *states.BaseStateImpl, params ...interface{}) states.IState
 }
 
 func (state payToSellerState) Process(ctx context.Context, iFrame frame.IFrame) {
-	panic("implementation required")
+	if iFrame.Header().KeyExists(string(frame.HeaderSubpackage)) {
+		subpkg, ok := iFrame.Header().Value(string(frame.HeaderSubpackage)).(*entities.Subpackage)
+		if !ok {
+			logger.Err("iFrame.Header() not a subpackage, frame: %v, %s state ", iFrame, state.Name())
+			return
+		}
+
+		var releaseStockAction *entities.Action
+		if err := state.settlementStock(ctx, subpkg); err != nil {
+			releaseStockAction = &entities.Action{
+				Name:      stock_action.Settlement.ActionName(),
+				Type:      actions.Stock.ActionName(),
+				Result:    string(states.ActionFail),
+				Reasons:   nil,
+				CreatedAt: time.Now().UTC(),
+			}
+		} else {
+			releaseStockAction = &entities.Action{
+				Name:      stock_action.Settlement.ActionName(),
+				Type:      actions.Stock.ActionName(),
+				Result:    string(states.ActionSuccess),
+				Reasons:   nil,
+				CreatedAt: time.Now().UTC(),
+			}
+		}
+
+		state.UpdateSubPackage(ctx, subpkg, releaseStockAction)
+		_, err := global.Singletons.SubPkgRepository.Update(ctx, *subpkg)
+		if err != nil {
+			logger.Err("SubPkgRepository.Update in %s state failed, orderId: %d, sellerId: %d, itemId: %d, error: %s",
+				state.Name(), subpkg.OrderId, subpkg.SellerId, subpkg.ItemId, err.Error())
+		} else {
+			logger.Audit("Cancel by seller success, orderId: %d, sellerId: %d, itemId: %d", subpkg.OrderId, subpkg.SellerId, subpkg.ItemId)
+		}
+	} else {
+		logger.Err("iFrame.Header() not a subpackage or sellerId not found, state: %s iframe: %v", state.Name(), iFrame)
+	}
 }
 
-//func (payToSeller payToSellerState) ProcessOrder(ctx context.Context, order entities.Order, itemsId []uint64, param interface{}) future.IFuture {
-//
-//	logger.Audit("Pay to Seller step, orderId: %d", order.OrderId)
-//
-//	if len(order.Items) == len(itemsId) {
-//		payToSeller.UpdateAllOrderStatus(ctx, &order, itemsId, states.OrderClosedStatus, false)
-//	} else {
-//		payToSeller.UpdateAllOrderStatus(ctx, &order, itemsId, states.OrderInProgressStatus, false)
-//	}
-//
-//	payToSeller.updateOrderItemsProgress(ctx, &order, itemsId, Delivered, true, states.OrderClosedStatus)
-//	if err := payToSeller.persistOrder(ctx, &order); err != nil {
-//		returnChannel := make(chan future.IDataFuture, 1)
-//		defer close(returnChannel)
-//		returnChannel <- future.IDataFuture{Data: nil, Ex: future.FutureError{Code: future.InternalError, Reason: "Unknown Error"}}
-//		return future.NewFuture(returnChannel, 1, 1)
-//	}
-//	returnChannel := make(chan future.IDataFuture, 1)
-//	defer close(returnChannel)
-//	returnChannel <- future.IDataFuture{Data: nil, Ex: nil}
-//	return future.NewFuture(returnChannel, 1, 1)
-//}
-//
-//func (payToSeller payToSellerState) persistOrder(ctx context.Context, order *entities.Order) error {
-//	_, err := global.Singletons.OrderRepository.Save(*order)
-//	if err != nil {
-//		logger.Err("OrderRepository.Save in %s step failed, order: %v, error: %s", payToSeller.Name(), order, err.Error())
-//	}
-//	return err
-//}
-//
-//func (payToSeller payToSellerState) updateOrderItemsProgress(ctx context.Context, order *entities.Order, itemsId []uint64, action string, result bool, itemStatus string) {
-//
-//	findFlag := false
-//	if itemsId != nil && len(itemsId) > 0 {
-//		for _, id := range itemsId {
-//			findFlag = false
-//			for i := 0; i < len(order.Items); i++ {
-//				if order.Items[i].ItemId == id {
-//					payToSeller.doUpdateOrderItemsProgress(ctx, order, i, action, result, itemStatus)
-//					findFlag = true
-//					break
-//				}
-//			}
-//
-//			if findFlag == false {
-//				logger.Err("%s received itemId %d not exist in order, orderId: %d", payToSeller.Name(), id, order.OrderId)
-//			}
-//		}
-//	} else {
-//		for i := 0; i < len(order.Items); i++ {
-//			payToSeller.doUpdateOrderItemsProgress(ctx, order, i, action, result, itemStatus)
-//		}
-//	}
-//}
-//
-//func (payToSeller payToSellerState) doUpdateOrderItemsProgress(ctx context.Context, order *entities.Order, index int,
-//	actionName string, result bool, itemStatus string) {
-//
-//	order.Items[index].Status = itemStatus
-//	order.Items[index].UpdatedAt = time.Now().UTC()
-//
-//	length := len(order.Items[index].Progress.StepsHistory) - 1
-//
-//	if order.Items[index].Progress.StepsHistory[length].ActionHistory == nil || len(order.Items[index].Progress.StepsHistory[length].ActionHistory) == 0 {
-//		order.Items[index].Progress.StepsHistory[length].ActionHistory = make([]entities.Action, 0, 5)
-//	}
-//
-//	action := entities.Action{
-//		Name:      actionName,
-//		Result:    result,
-//		CreatedAt: order.Items[index].UpdatedAt,
-//	}
-//
-//	order.Items[index].Progress.StepsHistory[length].ActionHistory = append(order.Items[index].Progress.StepsHistory[length].ActionHistory, action)
-//}
-//
-////
-////import (
-////	"github.com/Shopify/sarama"
-////	"gitlab.faza.io/order-project/order-service"
-////)
-////
-////func PayToSellerMessageValidate(message *sarama.ConsumerMessage) (*sarama.ConsumerMessage, error) {
-////	mess, err := main.CheckOrderKafkaAndMongoStatus(message, main.PayToSeller)
-////	if err != nil {
-////		return mess, err
-////	}
-////	return message, nil
-////}
-////
-////func PayToSellerAction(message *sarama.ConsumerMessage) error {
-////
-////	err := PayToSellerProduce("", []byte{})
-////	if err != nil {
-////		return err
-////	}
-////	return nil
-////}
-////
-////func PayToSellerProduce(topic string, payload []byte) error {
-////	return nil
-////}
+func (state payToSellerState) settlementStock(ctx context.Context, subpackage *entities.Subpackage) error {
+
+	var inventories = make(map[string]int, 32)
+	for z := 0; z < len(subpackage.Items); z++ {
+		item := subpackage.Items[z]
+		inventories[item.InventoryId] = int(item.Quantity)
+	}
+
+	iFuture := global.Singletons.StockService.BatchStockActions(ctx, inventories,
+		stock_action.New(stock_action.Settlement))
+	futureData := iFuture.Get()
+	if futureData.Error() != nil {
+		logger.Err("Settlement stock from stockService failed, state: %s, orderId: %d, sellerId: %d, itemId: %d, error: %s",
+			state.Name(), subpackage.OrderId, subpackage.SellerId, subpackage.ItemId, futureData.Error())
+		return futureData.Error().Reason()
+	}
+
+	logger.Audit("Settlement stock success, state: %s, orderId: %d, sellerId: %d, itemId: %d",
+		state.Name(), subpackage.OrderId, subpackage.SellerId, subpackage.ItemId)
+	return nil
+}
